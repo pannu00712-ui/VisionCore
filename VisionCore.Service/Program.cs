1d69692fc164c6b1b06549c40591701edf9d2993
pagalannu <pagalannu@gmail.com>
2026-06-22 13:08:29 +0500

    Fix Program.cs: restore Windows Service entry point

--- VisionCore.Service/Program.cs
+++ VisionCore.Service/Program.cs
@@ -1,53 +1,16 @@
-using Serilog;
-using Serilog.Events;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using VisionCore.Core;
-using VisionCore.Service;
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-    .Enrich.FromLogContext()
-    .WriteTo.Console(outputTemplate:
-        "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext}: {Message:lj}{NewLine}{Exception}")
-    .WriteTo.File(
-        path: System.IO.Path.Combine(
-            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-            "VisionCore", "logs", "service-bootstrap-.log"),
-        rollingInterval: RollingInterval.Day,
-        retainedFileCountLimit: 3)
-    .CreateBootstrapLogger();
-
-try
-{
-    Log.Information("VisionCore Service starting up (PID {Pid}).", Environment.ProcessId);
-
-    var builder = Host.CreateApplicationBuilder(args);
-
-    builder.Services.AddWindowsService(options =>
+IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService(options =>
     {
-        options.ServiceName = ServiceConstants.ServiceName;
-    });
+        options.ServiceName = "VisionCore Service";
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddVisionCoreServices();
+    })
+    .Build();
 
-    builder.Services.AddSerilog((services, lc) => lc
-        .ReadFrom.Configuration(builder.Configuration)
-        .ReadFrom.Services(services)
-        .Enrich.FromLogContext());
-
-    builder.Services.AddVisionCoreServices();
-    builder.Services.AddHostedService<VisionCoreWorker>();
-
-    var host = builder.Build();
-    await host.RunAsync();
-
-    Log.Information("VisionCore Service stopped cleanly.");
-    return 0;
-}
-catch (Exception ex) when (ex is not OperationCanceledException
-                           && ex.GetType().Name != "StopTheHostException")
-{
-    Log.Fatal(ex, "VisionCore Service terminated unexpectedly.");
-    return 1;
-}
-finally
-{
-    await Log.CloseAndFlushAsync();
-}
+await host.RunAsync();
